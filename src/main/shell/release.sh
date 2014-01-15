c1205d977b8fe93e93faee76c86ef7a01292b009
Brian Pontarelli <brian@inversoft.com>
2014-01-14 17:17:44 -0700

    Fixing bugs and getting savant to be self building

--- src/main/shell/release.sh
+++ src/main/shell/release.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-cp build/jars/*.jar ~/dev/inversoft/repositories/savant/org/savantbuild/plugin/groovy/0.1.0
-cp ~/dev/inversoft/repositories/savant/org/testng/testng/6.8.0/testng-6.8.0.jar.amd ~/dev/inversoft/repositories/savant/org/savantbuild/plugin/groovy/0.1.0/groovy-0.1.0.jar.amd
-cd ~/dev/inversoft/repositories/savant/org/savantbuild/plugin/groovy/0.1.0
-md5sum groovy-0.1.0.jar > groovy-0.1.0.jar.md5
-md5sum groovy-0.1.0.jar.amd > groovy-0.1.0.jar.amd.md5
-md5sum groovy-0.1.0-src.jar > groovy-0.1.0-src.jar.md5
+mkdir -p ~/.savant/cache/org/savantbuild/plugin/dependency/0.1.0-\{integration\}/
+cp build/jars/*.jar ~/.savant/cache/org/savantbuild/plugin/dependency/0.1.0-\{integration\}/
+cp src/main/resources/amd.xml ~/.savant/cache/org/savantbuild/plugin/dependency/0.1.0-\{integration\}/dependency-0.1.0-\{integration\}.jar.amd
+cd ~/.savant/cache/org/savantbuild/plugin/dependency/0.1.0-\{integration\}/
+md5sum dependency-0.1.0-\{integration\}.jar > dependency-0.1.0-\{integration\}.jar.md5
+md5sum dependency-0.1.0-\{integration\}.jar.amd > dependency-0.1.0-\{integration\}.jar.amd.md5
+md5sum dependency-0.1.0-\{integration\}-src.jar > dependency-0.1.0-\{integration\}-src.jar.md5
