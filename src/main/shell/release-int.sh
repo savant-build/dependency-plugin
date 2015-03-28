19984d0c94aa39ed7131e0d828db16f4280e75e9
Brian Pontarelli <brian@inversoft.com>
2015-03-28 13:04:04 -0600

    Cleaning up IJ files and updating to Savant 0.4.x

--- src/main/shell/release-int.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-name="dependency"
-version="0.2.0-{integration}"
-cp src/main/resources/amd.xml ~/.savant/cache/org/savantbuild/plugin/${name}/${version}/${name}-${version}.jar.amd
-cd ~/.savant/cache/org/savantbuild/plugin/${name}/${version}/
-md5sum ${name}-${version}.jar > ${name}-${version}.jar.md5
-md5sum ${name}-${version}.jar.amd > ${name}-${version}.jar.amd.md5
-md5sum ${name}-${version}-src.jar > ${name}-${version}-src.jar.md5
